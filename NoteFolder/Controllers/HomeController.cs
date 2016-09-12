0848cbf350894046206a4dc3198eaa0af3a3c6b8
derrickcreamer <derrickcreamer@gmail.com>
2016-09-12 00:59:38 -0400

    Create project files (mostly default). Add initial File model.

---
+++ NoteFolder/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NoteFolder.Models;
+
+namespace NoteFolder.Controllers {
+	public class HomeController : Controller {
+		public ActionResult Index() => View();
+	}
+}
---
+++ NoteFolder/Models/File.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NoteFolder.Models {
+	public class FileDbContext : DbContext {
+		public DbSet<File> Files { get; set; }
+	}
+
+	public class File {
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Text { get; set; }
+		public bool IsFolder { get; set; }
+		public DateTime TimeCreated {get; set; }
+		public DateTime TimeLastEdited { get; set; }
+
+		public virtual File Parent { get; set; }
+		public virtual ICollection<File> Children { get; set; }
+	}
+}
