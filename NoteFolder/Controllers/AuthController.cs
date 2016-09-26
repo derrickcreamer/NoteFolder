99ed4a0a37848879b1f4935d54463a12e6799036
derrickcreamer <derrickcreamer@gmail.com>
2016-09-26 03:21:25 -0400

    Add user registration functionality. Improve the way in which LastAction is displayed.

--- NoteFolder/Controllers/AuthController.cs
+++ NoteFolder/Controllers/AuthController.cs
@@ -27,18 +27,41 @@ namespace NoteFolder.Controllers {
 				ModelState.AddModelError("", "Invalid email or password. Please try again.");
 				return View(login);
 			}
-			var idCookie = UserManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
-			Request.GetOwinContext().Authentication.SignIn(idCookie);
+			SignIn(user);
 			return RedirectToLocal(login.ReturnURL); //todo: Perhaps this should redirect to user page instead.
 		}
 		protected ActionResult RedirectToLocal(string returnURL) {
 			if(string.IsNullOrWhiteSpace(returnURL) || !Url.IsLocalUrl(returnURL)) return RedirectToAction("Index", "Home");
 			else return Redirect(returnURL);
 		}
+		protected void SignIn(IdentityUser user) {
+			var idCookie = UserManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+			Request.GetOwinContext().Authentication.SignIn(idCookie);
+		}
+
 		[HttpPost]
 		public ActionResult LogOut() {
 			Request.GetOwinContext().Authentication.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+			TempData["LastAction"] = "You have successfully logged out.";
 			return RedirectToAction("Index", "Home");
+		}
+
+		[AllowAnonymous]
+		public ActionResult Register(string returnURL) => View(new LogInVM { ReturnURL = returnURL });
+
+		[AllowAnonymous]
+		[HttpPost]
+		public ActionResult Register(LogInVM register) { //todo: Take another look at whether register & login VMs are the same.
+			if(!ModelState.IsValid) return View(register);
+			var user = new IdentityUser { UserName = register.Email };
+			var result = UserManager.Create(user, register.Password);
+			if(!result.Succeeded) {
+				foreach(var error in result.Errors) ModelState.AddModelError("", error);
+				return View(register);
+			}
+			SignIn(user);
+			TempData["LastAction"] = "Account created!";
+			return RedirectToLocal(register.ReturnURL);
 		}
 	}
 }
